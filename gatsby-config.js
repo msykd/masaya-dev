72be7e2161f1c65f4c55d76ca87d08e14371272d
Masaya Kudo <m-kudo@monooq.com>
2019-03-04 15:39:50 +0900

    first commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `masaya dev`,
+    description: `developer masaya's site`,
+    author: `@msykd`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
